f98d7b25aa1232ba827b607fb8be7654a21f8651
Bea <bea@wemogy.com>
2020-08-03 13:44:34 +0200

    Updates

---
+++ precache-manifest.093f9b387a04dc61b64a16a0a5684a0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e8146dbe484a208d355cd60cb5be26",
+    "url": "/todoList-react-typescript/index.html"
+  },
+  {
+    "revision": "7c5c702b32daaa25e2be",
+    "url": "/todoList-react-typescript/static/js/2.49ef2f89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoList-react-typescript/static/js/2.49ef2f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c55ea2e2314c5269c8",
+    "url": "/todoList-react-typescript/static/js/main.d8878014.chunk.js"
+  },
+  {
+    "revision": "d967d1378fed151ca8bc",
+    "url": "/todoList-react-typescript/static/js/runtime-main.aeefd440.js"
+  }
+]);
